593b345454898df68a26eaf54eb46e196c846fd7
Theodore S. Lindsey <theodore.lindsey@gmail.com>
2015-03-07 23:55:31 -0600

    removed checklist and job.c include

--- main.c
+++ main.c
@@ -13,32 +13,8 @@
 
 using namespace std;
 
-/*
- * [x] Run executables without arguments (10)
- * [x] Run executables with arguments (10)
- * [x] set for HOME and PATH work properly (5)
- * [x] exit and quit work properly (5)
- * [x] cd (with and without arguments) works properly (5)
- * [x] PATH works properly. Give error messages when the executable is not found (10)
- * [x] Child processes inherit the environment (5)
- * [x] Allow background/foreground execution (&) (5)
- * [ ] Printing/reporting of background processes, (including the jobs command) (10)
- * [ ] Allow file redirection (> and <) (5)
- * [ ] Allow (1) pipe (|) (10)
- * [ ] Supports reading commands from prompt and from file (10)
- * [ ] Report (10)
- *
- * Bonus points:
- * [ ] Support multiple pipes in one command (10)
- * [ ] kill command delivers signals to background processes. The kill
- * command has the format: kill SIGNUM, JOBID, where SIGNUM is an integer
- * specifying the signal number, and JOBID is an integer that specifies
- * the job that should receive the signal (5)
- */
-
 #include "execute.c"
 #include "cd.c"
-#include "job.c"
 
 int fd;
 
